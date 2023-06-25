aec0ca03ecd8d687e9c2d2394858ec02a5f770f9
Qishuai Liu <liuqs1216@gmail.com>
2023-06-25 16:08:37 +0800

    add config for lunar.

---
+++ config/config.lua
@@ -0,0 +1,30 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+vim.opt.cmdheight = 2             -- more space in the neovim command line for displaying messages
+vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.shiftwidth = 4            -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2               -- insert 2 spaces for a tab
+vim.opt.relativenumber = true     -- relative line numbers
+vim.opt.wrap = true               -- wrap lines
+
+-- git blame related
+vim.g.gitblame_message_template = '<author> • <summary> • <date>'
+vim.g.gitblame_delay = 1000
+vim.g.gitblame_date_format = "%b"
+
+-- telescope
+local builtin = require('telescope/builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-s>', builtin.live_grep, {})
+
+-- lvim.colorscheme = "darcula"
+lvim.colorscheme = "gruvbox"
+lvim.format_on_save = true
+
+lvim.plugins = {
+    { "doums/darcula" },
+    { "morhetz/gruvbox" },
+    { "f-person/git-blame.nvim" },
+}
