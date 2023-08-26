9689ca5dd07d1aff35c50fa911855a653e17eea8
Qishuai Liu <liuqs1216@gmail.com>
2023-08-26 11:30:58 +0100

    add search for current file in lunarvim.

--- config/config.lua
+++ config/config.lua
@@ -15,9 +15,18 @@ vim.g.gitblame_delay = 1000
 vim.g.gitblame_date_format = "%b"
 
 -- telescope
+-- require('telescope.builtin').grep_string({vim.fn.expand("%:p")})
+lvim.builtin.which_key.mappings["ss"] = {
+    function ()
+        require("telescope/builtin").current_buffer_fuzzy_find()
+    end,
+  "search current file",
+}
+
 local builtin = require('telescope/builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-s>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "find files." })
+vim.keymap.set('n', '<C-s>', builtin.live_grep, { desc = "find words in whole project" })
+-- vim.keymap.set('n', '<C-w>', builtin.grep_string, { desc = "search in current file" })
 
 lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
 lvim.builtin.telescope.defaults.initial_mode = "insert"
@@ -40,6 +49,13 @@ lvim.builtin.telescope.pickers = {
         },
     },
     live_grep = {
+        layout_config = {
+            width = 0.95,
+            height = 0.95,
+            preview_width = 0.6,
+        },
+    },
+    current_buffer_fuzzy_find = {
         layout_config = {
             width = 0.95,
             height = 0.95,
