28ce66b82312b55f3a94f94d844657bf824f4fda
Qishuai Liu <liuqs1216@gmail.com>
2023-08-28 05:25:44 +0800

    update dashboard banner.

--- config/config.lua
+++ config/config.lua
@@ -22,6 +22,12 @@ lvim.builtin.which_key.mappings["ss"] = {
     end,
     "search current file",
 }
+lvim.builtin.which_key.mappings["sw"] = {
+    function()
+        require("telescope/builtin").buffers()
+    end,
+    "search file in buffers",
+}
 
 local builtin = require('telescope/builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "find files." })
@@ -40,43 +46,35 @@ lvim.builtin.terminal.open_mapping = "<C-\\>"
 lvim.builtin.terminal.direction = "vertical"
 lvim.builtin.terminal.size = 70
 
+
+lvim.builtin.telescope.pickers = {
+    find_files = {
+        layout_config = {
+            width = 0.95,
+            height = 0.95,
+            preview_width = 0.6,
+        },
+    },
+    live_grep = {
+        layout_config = {
+            width = 0.95,
+            height = 0.95,
+            preview_width = 0.6,
+        },
+        only_sort_text = true,
+    },
+    current_buffer_fuzzy_find = {
+        layout_config = {
+            width = 0.95,
+            height = 0.95,
+            preview_width = 0.6,
+        },
+    },
+}
+
 -- dashboard
--- local function banner()
---     local banner1 = {
---         [[       QQQQQQQQQ       iiii                  hhhhhhh                                                   iiii  ]],
---         [[     QQ:::::::::QQ    i::::i                 h:::::h                                                  i::::i ]],
---         [[   QQ:::::::::::::QQ   iiii                  h:::::h                                                   iiii  ]],
---         [[  Q:::::::QQQ:::::::Q                        h:::::h                                                         ]],
---         [[  Q::::::O   Q::::::Qiiiiiii     ssssssssss   h::::h hhhhh       uuuuuu    uuuuuu    aaaaaaaaaaaaa   iiiiiii ]],
---         [[  Q:::::O     Q:::::Qi:::::i   ss::::::::::s  h::::hh:::::hhh    u::::u    u::::u    a::::::::::::a  i:::::i ]],
---         [[  Q:::::O     Q:::::Q i::::i ss:::::::::::::s h::::::::::::::hh  u::::u    u::::u    aaaaaaaaa:::::a  i::::i ]],
---         [[  Q:::::O     Q:::::Q i::::i s::::::ssss:::::sh:::::::hhh::::::h u::::u    u::::u             a::::a  i::::i ]],
---         [[  Q:::::O     Q:::::Q i::::i  s:::::s  ssssss h::::::h   h::::::hu::::u    u::::u      aaaaaaa:::::a  i::::i ]],
---         [[  Q:::::O     Q:::::Q i::::i    s::::::s      h:::::h     h:::::hu::::u    u::::u    aa::::::::::::a  i::::i ]],
---         [[  Q:::::O  QQQQ:::::Q i::::i       s::::::s   h:::::h     h:::::hu::::u    u::::u   a::::aaaa::::::a  i::::i ]],
---         [[  Q::::::O Q::::::::Q i::::i ssssss   s:::::s h:::::h     h:::::hu:::::uuuu:::::u  a::::a    a:::::a  i::::i ]],
---         [[  Q:::::::QQ::::::::Qi::::::is:::::ssss::::::sh:::::h     h:::::hu:::::::::::::::uua::::a    a:::::a i::::::i]],
---         [[   QQ::::::::::::::Q i::::::is::::::::::::::s h:::::h     h:::::h u:::::::::::::::ua:::::aaaa::::::a i::::::i]],
---         [[     QQ:::::::::::Q  i::::::i s:::::::::::ss  h:::::h     h:::::h  uu::::::::uu:::u a::::::::::aa:::ai::::::i]],
---         [[       QQQQQQQQ::::QQiiiiiiii  sssssssssss    hhhhhhh     hhhhhhh    uuuuuuuu  uuuu  aaaaaaaaaa  aaaaiiiiiiii]],
---         [[               Q:::::Q                                                                                       ]],
---         [[                QQQQQQ                                                                                       ]],
---     }
---     local banner2 = {
---         [[ test ]],
---     }
 
---     local alpha_wins = vim.tbl_filter(function(win)
---         local buf = vim.api.nvim_win_get_buf(win)
---         return vim.api.nvim_buf_get_option(buf, "filetype") == "alpha"
---     end, vim.api.nvim_list_wins())
-
---     if vim.api.nvim_win_get_height(alpha_wins[#alpha_wins]) < 36 then
---         return banner1
---     end
---     return banner2
--- end
-lvim.builtin.alpha.dashboard.section.header.val = {
+local banner1 = {
     [[       QQQQQQQQQ       iiii                  hhhhhhh                                                   iiii  ]],
     [[     QQ:::::::::QQ    i::::i                 h:::::h                                                  i::::i ]],
     [[   QQ:::::::::::::QQ   iiii                  h:::::h                                                   iiii  ]],
@@ -96,41 +94,32 @@ lvim.builtin.alpha.dashboard.section.header.val = {
     [[               Q:::::Q                                                                                       ]],
     [[                QQQQQQ                                                                                       ]],
 }
--- lvim.builtin.alpha.dashboard.section.header.val = {
---     [[       ██████     ███          █████                            ███  ]],
---     [[     ███░░░░███  ░░░          ░░███                            ░░░   ]],
---     [[    ███    ░░███ ████   █████  ░███████   █████ ████  ██████   ████  ]],
---     [[   ░███     ░███░░███  ███░░   ░███░░███ ░░███ ░███  ░░░░░███ ░░███  ]],
---     [[   ░███   ██░███ ░███ ░░█████  ░███ ░███  ░███ ░███   ███████  ░███  ]],
---     [[   ░░███ ░░████  ░███  ░░░░███ ░███ ░███  ░███ ░███  ███░░███  ░███  ]],
---     [[    ░░░██████░██ █████ ██████  ████ █████ ░░████████░░████████ █████ ]],
---     [[      ░░░░░░ ░░ ░░░░░ ░░░░░░  ░░░░ ░░░░░   ░░░░░░░░  ░░░░░░░░ ░░░░░  ]],
--- }
-lvim.builtin.alpha.dashboard.section.footer.val = "天下事有难易乎"
-
-lvim.builtin.telescope.pickers = {
-    find_files = {
-        layout_config = {
-            width = 0.95,
-            height = 0.95,
-            preview_width = 0.6,
-        },
-    },
-    live_grep = {
-        layout_config = {
-            width = 0.95,
-            height = 0.95,
-            preview_width = 0.6,
-        },
-    },
-    current_buffer_fuzzy_find = {
-        layout_config = {
-            width = 0.95,
-            height = 0.95,
-            preview_width = 0.6,
-        },
-    },
+local banner2 = {
+    [[       ██████     ███          █████                            ███  ]],
+    [[     ███░░░░███  ░░░          ░░███                            ░░░   ]],
+    [[    ███    ░░███ ████   █████  ░███████   █████ ████  ██████   ████  ]],
+    [[   ░███     ░███░░███  ███░░   ░███░░███ ░░███ ░███  ░░░░░███ ░░███  ]],
+    [[   ░███   ██░███ ░███ ░░█████  ░███ ░███  ░███ ░███   ███████  ░███  ]],
+    [[   ░░███ ░░████  ░███  ░░░░███ ░███ ░███  ░███ ░███  ███░░███  ░███  ]],
+    [[    ░░░██████░██ █████ ██████  ████ █████ ░░████████░░████████ █████ ]],
+    [[      ░░░░░░ ░░ ░░░░░ ░░░░░░  ░░░░ ░░░░░   ░░░░░░░░  ░░░░░░░░ ░░░░░  ]],
 }
+lvim.builtin.alpha.dashboard.section.header.val = function()
+    local alpha_wins = vim.tbl_filter(function(win)
+        local buf = vim.api.nvim_win_get_buf(win)
+        return vim.api.nvim_buf_get_option(buf, "filetype") == "alpha"
+    end, vim.api.nvim_list_wins())
+
+    if vim.api.nvim_win_get_height(alpha_wins[#alpha_wins]) < 36 then
+        return banner2
+    end
+    return banner1
+end
+lvim.builtin.alpha.dashboard.section.footer.val = "天下事有难易乎"
+-- lvim.builtin.alpha.dashboard.section.footer.val = require "lvim.interface.text".align_center({ width = 0 }, {
+--     "talk is expensive, hide your code",
+--     "天下事有难易乎",
+-- }, 0.5)
 
 -- lvim.colorscheme = "darcula"
 lvim.colorscheme = "gruvbox-material"
