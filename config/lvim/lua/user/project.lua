d7df08bceaf73f36549c2846ab7d3f2ee43be95f
Qishuai Liu <liuqs1216@gmail.com>
2023-12-02 08:34:38 +0000

    support manual mode of project.vim.

--- config/lvim/lua/user/project.lua
+++ config/lvim/lua/user/project.lua
@@ -1,2 +1,3 @@
 -- project.nvim config
 lvim.builtin.project.patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" , "compile_commands.json"}
+lvim.builtin.project.manual_mode = true
