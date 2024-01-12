cb9394293b218a6fe270a4c2ca9b47386af73c2b
Qishuai Liu <liuqs1216@gmail.com>
2024-01-12 14:35:48 +0800

    add clipboard related setting for lvim.

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,17 +2,21 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-vim.opt.cmdheight = 2             -- more space in the neovim command line for displaying messages
-vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
-vim.opt.shiftwidth = 4            -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2               -- insert 2 spaces for a tab
-vim.opt.relativenumber = true     -- relative line numbers
-vim.opt.wrap = true               -- wrap lines
+vim.opt.cmdheight = 2                               -- more space in the neovim command line for displaying messages
+vim.opt.guifont = "monospace:h17"                   -- the font used in graphical neovim applications
+vim.opt.shiftwidth = 4                              -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2                                 -- insert 2 spaces for a tab
+vim.opt.relativenumber = true                       -- relative line numbers
+vim.opt.wrap = true                                 -- wrap lines
+vim.api.nvim_set_option("clipboard", "unnamedplus") -- needs xclips
 
 -- git blame related
 vim.g.gitblame_message_template = '<author> • <summary> • <date>'
 vim.g.gitblame_delay = 1000
 vim.g.gitblame_date_format = "%b"
+
+-- leetcode
+vim.g.leetcode_browser = "chrome"
 
 -- telescope
 require("user.telescope")
@@ -57,5 +61,8 @@ lvim.plugins = {
         config = function()
             vim.fn["mkdp#util#install"]()
         end,
+    },
+    {
+        "mbledkowski/neuleetcode.vim"
     },
 }
