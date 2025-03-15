ee3a0d08ffe26fba55fa2e56be228efbbfe421d4
Qishuai Liu <liuqs1216@gmail.com>
2025-03-15 19:37:44 +0800

    update lazynvim.

--- config/lazynvim/lua/config/options.lua
+++ config/lazynvim/lua/config/options.lua
@@ -1,7 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd", "comile_commands.json" }
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd", "compile_commands.json" }
 vim.opt.wrap = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.lsp.set_log_level("off")
 vim.g.format_enabled = false
 vim.opt.jumpoptions = "stack"
